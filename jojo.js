11f4e6b72e5e4ddff8899e00d5a2fa3510f02a8e
DenisAtuti <atutidennis@gmail.com>
2017-11-25 21:19:59 +0300

    update2

--- jojo.js
+++ jojo.js
@@ -1,5 +1,4 @@
 autoType(".type-j",200);
-autoTyp(".type-j",200);
 var myIndex = 0;
 carousel();
 
@@ -42,29 +41,42 @@ function autoType(elementClass, typingSpeed){
   },1500);
 }
 
-function autoTyp(elementClass, typingSpeed){
-  var thhis = $(elementClass);
-  thhis.css({
-    "position": "relative",
-    "display": "inline-block"
-  });
-  thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
-  thhis = thhis.find(".text");
-  var text = thhis.text().trim().split('');
-  var amntOfChars = text.length;
-  var newString = "";
-  thhis.text("|");
-  setTimeout(function(){
-    thhis.css("opacity",1);
-    thhis.prev().removeAttr("style");
-    thhis.text("");
-    for(var i = 0; i < amntOfChars; i++){
-      (function(i,char){
-        setTimeout(function() {        
-          newString += char;
-          thhis.text(newString);
-        },i*typingSpeed);
-      })(i+1,text[i]);
+    var incrementScroll = function() {
+        window.scrollBy(0, 200) ;
     }
-  },1500);
+
+    var scrollLoopId;
+    var startScrollLoop = function() {
+        scrollLoopId = setInterval( incrementScroll, 95000) ; 
+    }
+
+    var stopScrollLoop = function() {
+        clearInterval( scrollLoopId ) ;
+    }
+
+    function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+function comparison(){
+  if(getWidth() < 520){
+    stopScrollLoop();
+  }else{
+    startScrollLoop();
+  }
 }
